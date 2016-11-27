8e1e2f079875d726bf0bbd8853c8a5ac32072dfe
TheIncognitoZ <smitesh.sawant@student.csulb.edu>
2016-11-26 17:11:49 -0800

    Updated GET and POST to server

---
+++ Phase 3/MyApplication1/app/src/main/java/com/the_incognito/darry/myapplication1/Person.java
@@ -0,0 +1,36 @@
+package com.the_incognito.darry.myapplication1;
+
+/**
+ * Created by darry on 11/23/2016.
+ */
+public class Person {
+    private String author;
+    private String content;
+    private String receiver;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+}
